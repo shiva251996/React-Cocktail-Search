322c8ef2dc464826f5412367e68ee6da5b5bcb4a
shiva251996 <40624533+shiva251996@users.noreply.github.com>
2021-09-05 17:11:46 +0530

    Add files via upload

---
+++ searching cocktails/src/context.js
@@ -0,0 +1,61 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      const { drinks } = data;
+      // console.log(drinks);
+
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { iDDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: iDDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks]);
+
+  return (
+    <AppContext.Provider
+      value={{ loading, searchTerm, cocktails, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
